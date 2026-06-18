f471e0ba4b295fd21e119100eeae59643960d6f6
NotSwedishBacon <mbacon89@gmail.com>
2026-06-18 09:43:26 +0200

    install discord in home directory

--- scripts/01-main.sh
+++ scripts/01-main.sh
@@ -23,25 +23,26 @@ flatpak install flathub -y org.telegram.desktop &&
 flatpak install flathub -y com.spotify.Client
 
 # Install official Discord client 
-sudo mkdir -p "/var/opt/discord"
-sudo mkdir -p "/usr/local/share/applications"
-sudo mkdir -p "/usr/local/share/icons/hicolor/256x256/apps"
+mkdir -p "$HOME/.local/share/discord"
+mkdir -p "$HOME/.local/share/icons/hicolor/256x256/apps"
+mkdir -p "$HOME/.local/share/applications"
 curl -L -o "$HOME/Downloads/discord.tar.gz" "https://discord.com/api/download?platform=linux&format=tar.gz"
-sudo tar -xvf "$HOME/Downloads/discord.tar.gz" -C "/var/opt/discord/" --strip-components=1
-ln -sf "/var/opt/discord/discord.png" "/usr/local/share/icons/hicolor/256x256/apps/discord.png"
-sudo tee "/usr/local/share/applications/discord.desktop" > /dev/null <<'EOF'
+tar -xvf "$HOME/Downloads/discord.tar.gz" -C "$HOME/.local/share/discord/" --strip-components=1
+ln -sf "$HOME/.local/share/discord/discord.png" "$HOME/.local/share/icons/hicolor/256x256/apps/discord.png"
+cat << EOF > "$HOME/.local/share/applications/discord.desktop"
 [Desktop Entry]
 Name=Discord
 StartupWMClass=discord
 Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
 GenericName=Internet Messenger
-Exec=/var/opt/discord/discord
+Exec=$HOME/.local/share/discord/discord
+Icon=discord
 Type=Application
 Categories=Network;InstantMessaging;
-Path=/var/opt/discord
+Path=$HOME/.local/share/discord
 EOF
-sudo chmod +x "/usr/local/share/applications/discord.desktop"
-sudo update-desktop-database "/usr/local/share/applications"
+chmod +x "$HOME/.local/share/applications/discord.desktop"
+update-desktop-database "$HOME/.local/share/applications"
 rm -f "$HOME/Downloads/discord.tar.gz"
 
 # Enable devmode and aurora-cli
