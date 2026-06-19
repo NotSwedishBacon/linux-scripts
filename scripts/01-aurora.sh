3ba62b9c5d270bb0feefc565d14f58c36febfa9d
NotSwedishBacon <mbacon89@gmail.com>
2026-06-19 21:02:51 +0200

    split the scripts into distro specific

---
+++ scripts/01-aurora.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Enable TMP2 unlocking
+ujust toggle-tpm2
+
+# Disable RAOP (Airplay)
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
+sudo tee "/etc/pipewire/pipewire.conf.d/disable-raop.conf" > /dev/null <<'EOF'
+context.properties = {
+    module.raop = false
+}
+EOF
+systemctl --user restart pipewire pipewire-pulse;
+
+# Install my flathub apps
+# KDE apps
+flatpak install flathub -y org.kde.krita &&
+# non KDE apps
+flatpak install flathub -y org.libreoffice.LibreOffice &&
+flatpak install flathub -y io.github.shiftey.Desktop &&
+flatpak install flathub -y com.prusa3d.PrusaSlicer &&
+flatpak install flathub -y org.telegram.desktop &&
+flatpak install flathub -y com.spotify.Client
+
+# Install official Discord client 
+mkdir -p "$HOME/.local/share/discord"
+mkdir -p "$HOME/.local/share/icons/hicolor/256x256/apps"
+mkdir -p "$HOME/.local/share/applications"
+curl -L -o "$HOME/Downloads/discord.tar.gz" "https://discord.com/api/download?platform=linux&format=tar.gz"
+tar -xvf "$HOME/Downloads/discord.tar.gz" -C "$HOME/.local/share/discord/" --strip-components=1
+ln -sf "$HOME/.local/share/discord/discord.png" "$HOME/.local/share/icons/hicolor/256x256/apps/discord.png"
+cat << EOF > "$HOME/.local/share/applications/discord.desktop"
+[Desktop Entry]
+Name=Discord
+StartupWMClass=discord
+Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
+GenericName=Internet Messenger
+Exec=$HOME/.local/share/discord/discord
+Icon=discord
+Type=Application
+Categories=Network;InstantMessaging;
+Path=$HOME/.local/share/discord
+EOF
+chmod +x "$HOME/.local/share/applications/discord.desktop"
+update-desktop-database "$HOME/.local/share/applications"
+rm -f "$HOME/Downloads/discord.tar.gz"
+
+# Enable devmode and aurora-cli
+ujust aurora-cli
+ujust devmode
+
+# Enable kargs (last to minimise initramfs gens)
+sudo tee "/etc/modprobe.d/i915.conf" > /dev/null <<'EOF'
+options i915 enable_guc=2
+options i915 enable_fbc=1
+EOF
+sudo rpm-ostree initramfs --enable --arg=-I --arg=/etc/modprobe.d/i915.conf
+
+echo "Time to reboot for all changes to take effect!"
