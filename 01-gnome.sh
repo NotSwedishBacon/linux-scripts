480b78ed5ea65703ba35ee86da311af29b1490c9
NotSwedishBacon <mbacon89@gmail.com>
2026-08-23 15:59:56 +0200

    Add files via upload

---
+++ 01-gnome.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Gnome minimise and maximise buttons
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+# Enable TMP2 unlocking
+sudo systemd-cryptenroll --tpm2-device=auto /dev/nvme0n1p3
+
+# Disable RAOP (Airplay)
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
+sudo tee "/etc/pipewire/pipewire.conf.d/disable-raop.conf" > /dev/null <<'EOF'
+context.properties = {
+    module.raop = false
+}
+EOF
+systemctl --user restart pipewire pipewire-pulse;
+
+# Setup flathub and remove fedora remotes
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-modify --enable flathub
+sudo flatpak uninstall --all -y
+sudo flatpak remote-delete fedora
+sudo flatpak remote-delete fedora-testing
+
+# Install my flathub apps
+# Gnome apps
+flatpak install flathub -y org.gnome.Calculator &&
+flatpak install flathub -y org.gnome.Calendar &&
+flatpak install flathub -y org.gnome.Extensions &&
+flatpak install flathub -y org.gnome.TextEditor &&
+flatpak install flathub -y org.gnome.Loupe &&
+flatpak install flathub -y org.gnome.Logs &&
+flatpak install flathub -y org.gnome.NautilusPreviewer &&
+flatpak install flathub -y org.gnome.Papers &&
+flatpak install flathub -y org.gnome.Weather &&
+# non gnome apps
+flatpak install flathub -y io.github.shiftey.Desktop &&
+flatpak install flathub -y com.prusa3d.PrusaSlicer &&
+flatpak install flathub -y org.telegram.desktop &&
+flatpak install flathub -y org.gimp.GIMP &&
+flatpak install flathub -y org.inkscape.Inkscape &&
+flatpak install flathub -y org.mozilla.firefox &&
+flatpak install flathub -y org.mozilla.thunderbird 
+
+# Disable built-in firefox
+sudo mkdir -p /usr/local/share/applications
+sudo cp /usr/share/applications/org.mozilla.firefox.desktop /usr/local/share/applications/
+sudo sed -i "2a\\NotShowIn=GNOME;KDE" /usr/local/share/applications/org.mozilla.firefox.desktop
+sudo update-desktop-database /usr/local/share/applications/
+
+# Install official Discord client
+mkdir -p "$HOME/.local/share/discord"
+mkdir -p "$HOME/.local/share/icons/hicolor/256x256/apps"
+mkdir -p "$HOME/.local/share/applications"
+curl -L -o "$HOME/Downloads/discord.tar.gz" "https://discord.com/api/download?platform=linux&format=tar.gz"
+tar -xvf "$HOME/Downloads/discord.tar.gz" -C "$HOME/.local/share/discord/" --strip-components=1
+ln -sf "$HOME/.local/share/discord/discord.png" "$HOME/.local/share/icons/hicolor/256x256/apps/discord.png"
+cat << EOF > "$HOME/.local/share/applications/discord.desktop"
+[Desktop Entry]
+Name=Discord
+StartupWMClass=discord
+Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
+GenericName=Internet Messenger
+Exec=$HOME/.local/share/discord/discord
+Icon=discord
+Type=Application
+Categories=Network;InstantMessaging;
+Path=$HOME/.local/share/discord
+EOF
+chmod +x "$HOME/.local/share/applications/discord.desktop"
+update-desktop-database "$HOME/.local/share/applications"
+rm -f "$HOME/Downloads/discord.tar.gz"
+
+echo "All done!"
