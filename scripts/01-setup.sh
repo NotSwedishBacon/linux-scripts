8d6089ed0af18a08089b854565c039d9a7d0929b
NotSwedishBacon <mbacon89@gmail.com>
2026-09-05 15:59:43 +0200

    Install VS Code and Distrobox in setup script
    Added installation steps for Visual Studio Code and Distrobox.

--- scripts/01-setup.sh
+++ scripts/01-setup.sh
@@ -72,4 +72,8 @@ chmod +x "$HOME/.local/share/applications/discord.desktop"
 update-desktop-database "$HOME/.local/share/applications"
 rm -f "$HOME/Downloads/discord.tar.gz"
 
+# Install VS Code and Distrobox
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo rpm-ostree install -y code distrobox
+
 echo "All done!"
